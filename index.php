7faed7949a209bc919a58542cfa96bf3e9b8eedc
Matheus Soares <theu2032@gmail.com>
2018-11-28 20:26:28 -0300

    Primeiro teste

---
+++ index.php
@@ -0,0 +1,16 @@
+﻿<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title> Help Tecnologia | Estamos aqui para tirar todas as suas duvidas sobre Tecnologia.</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+    </head>
+    
+    <body>
+    	<div id="Site">
+            <div id="Cabeçalho">
+                
+            </div>
+		</div>    
+    </body>
+</html>
